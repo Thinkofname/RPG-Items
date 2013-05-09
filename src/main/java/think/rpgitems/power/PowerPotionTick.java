8fb167e21b1967035bd3df7af52f52414dece1d9
thinkofdeath <purggames@gmail.com>
2013-05-09 16:26:46 +0100

    Fixed potiontick flickering

--- src/main/java/think/rpgitems/power/PowerPotionTick.java
+++ src/main/java/think/rpgitems/power/PowerPotionTick.java
@@ -10,28 +10,38 @@ import think.rpgitems.data.Locale;
 import think.rpgitems.power.types.PowerTick;
 
 public class PowerPotionTick extends Power implements PowerTick {
-    
+
     public int amplifier = 2;
     public PotionEffectType effect = PotionEffectType.SPEED;
 
     @Override
     public void tick(Player player) {
-        player.addPotionEffect(new PotionEffect(effect, 20, amplifier, true), false);
-        
+        boolean hasEffect = false;
+        for (PotionEffect potionEffect : player.getActivePotionEffects()) {
+            if (potionEffect.getType().equals(effect) && potionEffect.getDuration() == 25) {
+                player.addPotionEffect(new PotionEffect(effect, 30, amplifier, true), true);
+                hasEffect = true;
+                break;
+            }
+        }
+        if (!hasEffect) {
+            player.addPotionEffect(new PotionEffect(effect, 30, amplifier, true), true);
+        }
+
     }
 
     @Override
     public void init(ConfigurationSection s) {
         amplifier = s.getInt("amplifier");
         effect = PotionEffectType.getByName(s.getString("effect", "heal"));
-        
+
     }
 
     @Override
     public void save(ConfigurationSection s) {
         s.set("amplifier", amplifier);
         s.set("effect", effect.getName());
-        
+
     }
 
     @Override
